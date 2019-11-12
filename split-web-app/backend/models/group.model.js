d8b591e213846d5b7da1fd67dbfe651194c1be12
syan22 <47373714+syan22@users.noreply.github.com>
2019-11-12 18:03:23 -0500

    init

---
+++ split-web-app/backend/models/group.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const groupSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+
+}, {
+  timestamps: true,
+});
+
+const Group = mongoose.model('Group', groupSchema);
+
+module.exports = Group;
